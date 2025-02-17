705b8b1bee1f8e4bff5dca147a04e597da119315
Amit Verma <amitjsr8786@gmail.com>
2025-02-17 12:12:17 +0530

    Password-Generator

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,8 @@
-import { useState } from "react"
-
-
+import BgChanger from "./components/bgChanger"
 
 function App() {
-
-  const [color,SetColor]=useState("bg-blue-500")
-
   return (
-    <div className={`w-full relative h-screen ${color}`}>
-      <div className="flex flex-row gap-4 p-4 absolute bg-white items-center justify-center bottom-14  left-1/2 -translate-x-1/2 ">
-          <button className="px-2 py-4" onClick={()=>{
-            SetColor("bg-red-500")
-          }}>Red</button>
-            <button className="px-2 py-4 " onClick={()=>{
-            SetColor("bg-green-500")
-          }}>Green</button>
-      </div>
- </div>
+     <BgChanger/>
   )
 }
 
